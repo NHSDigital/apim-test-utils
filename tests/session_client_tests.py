4ef7b8286efb32c67a16ff6e7bf6e27168190bec
Alex Carrie <acarrie.dev@gmail.com>
2021-01-12 12:07:59 +0000

    APM-1710 Test for Time out exception

--- tests/session_client_tests.py
+++ tests/session_client_tests.py
@@ -46,3 +46,26 @@ async def test_explicit_uri_http_bin_post(api_client: APISessionClient):
         assert body['headers'].get('Host') == 'httpbin.org'
         assert body['headers'].get('Content-Type') == 'application/json'
         assert body['data'] == json.dumps(data)
+
+
+@pytest.mark.asyncio
+async def test_200_with_allow_retries(api_client: APISessionClient):
+    async with APISessionClient("https://httpbin.org") as session:
+        async with await session.get("get", allow_retries=True) as resp:
+            assert resp.status == 200
+
+
+# This test intentially takes at least 54 seconds to execute and might not be right for deploys
+@pytest.mark.asyncio
+async def test_429_with_allow_retries(api_client: APISessionClient):
+    async with APISessionClient("https://httpbin.org") as session:
+        with pytest.raises(TimeoutError) as excinfo:
+            await session.get("status/429", allow_retries=True)
+
+        error = excinfo.value
+        assert "Time out on 429 retries" in str(error)
+
+# Test other methods
+# Test other status'?
+# Test Functionality of _retry_429s in here?
+# How to simulate behaviour of different responses coming back?
